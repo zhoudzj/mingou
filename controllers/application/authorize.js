04560f891fb78f277f0ae02c5f1c4da0aa38a957
dimark <zhoudi@33.cn>
2020-05-26 19:37:03 +0800

    密码加盐

--- controllers/application/authorize.js
+++ controllers/application/authorize.js
@@ -1,4 +1,6 @@
 const {AppError} = require('ch-error');
+const Helper = require('ch-vendor/helper');
+
 const Cache = require('../../controllers/common/cache');
 const moment = require('moment');
 
@@ -12,36 +14,40 @@ module.exports = {
     async loginByPassword(ctx) {
         let attributes = ctx.request.body;
         console.log('==========',ctx.request.body);
-        let options = { name: attributes.userName, password: attributes.password };
-        if (attributes.type === 'email') options = { email: attributes.email };
+        let options = { name: attributes.userName };
         let user = await userModel.findOne({
             where: options,
             raw: true
         });
         if (!user) throw new AppError(11004, '用户不存在');
-        // let pwd = User.decryptPassword(attributes.password);
-        // let pwdHash = Helper.Md5(pwd, user.salt);
-        // if (pwdHash !== user.password) throw new AppError(11006, '密码错误');
+        let pwdHash = Helper.Md5(attributes.password, user.salt);
+        if (pwdHash !== user.password) throw new AppError(11006, '密码错误');
         console.log('==========',user);
         let token = await Cache.setTokenInfo(user);
         console.warn('xx==xx==',token);
         ctx.body.data = { token };
     },
+    //用户密码注册
     async registerByPassword(ctx) {
         console.log('==========',ctx.request.body);
         let attributes = ctx.request.body;
         let now = moment().unix();
+
+        let salt = Math.random().toString(32).substr(2, 10);
+        let pwdHash = Helper.Md5(attributes.password, salt);
+
         let result = await userModel.create({
             name: attributes.userName,
-            password: attributes.password,
+            password: pwdHash,
             type: userModel.TYPE_NORMAL,
+            salt,
             add_time:now
         });
         if (!result) throw new AppError(11004, '注册失败');
         ctx.body.data = {};
     },
     async getUserInfo(ctx) {
-        
+
     },
     //退出登录
     async signOut(ctx) {
