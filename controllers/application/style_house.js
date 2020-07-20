cd8d56db1bcf78e17e7e0b4c8b2e9f5ccbf237c9
zhoudzj@163.com <Mark@zhoudideMacBook-Pro-2.local>
2020-07-20 08:29:06 +0800

    修改户型选择

--- controllers/application/style_house.js
+++ controllers/application/style_house.js
@@ -3,17 +3,33 @@ const {AppError} = require('ch-error');
 const Mysql = global.Mysql;
 const Op = Mysql.Op;
 const styleHouseModel = Mysql.models.style;
+const houseModel = Mysql.models.community;
 const redis = global.Redis;
   
 module.exports = {
     //支持的token列表
-    async findByTypeId(ctx) {
+    async findType(ctx) {
         let attributes = ctx.attributes;
-        
+        if(!Array.isArray(attributes.roomData)){
+            throw new AppError(11010, '参数错误');
+       }
+       const newArr = attributes.roomData.map(item=>{
+            return Number(item)
+       })
+       console.log(newArr);
+        const typeObj = await houseModel.findOne({
+            attributes: ['type'],
+            where:{
+                building:newArr[0],
+                unit:newArr[1],
+                room:newArr[2]
+            }
+        });
+        console.log(typeObj);
         ctx.body.data = await styleHouseModel.findAll({
             attributes: ['id','type','name','img','typeId'],
             where: {
-                typeId: attributes.typeId,
+                type: typeObj.type,
             }
         });
     },
--- routes/application/style_house.js
+++ routes/application/style_house.js
@@ -5,12 +5,12 @@ module.exports = {
     routes: [           //路由列表(默认:[], 无路由)
         {
             description: '获取户型信息',    //资源描述(默认:'')
-            path: 'findByTypeId',           //请求路径(默认:'/')
+            path: 'findType',           //请求路径(默认:'/')
             method: 'post',         //请求方法(必填项, 支持get,post等标准方法)
             validator: {        //参数验证(默认:null. 依赖"ch-validator"模块, 欢迎提供意见,下为举例)
-                'typeId': ['户型分类编号', 'required', 'integer', {'min': 1}],
+                'roomData': ['房间号', 'required', 'array'],
             },
-            controller: ctl.findByTypeId    //可以使用预先加载的CONTROLLER_PATH中的函数, 当然也可以传入函数。
+            controller: ctl.findType    //可以使用预先加载的CONTROLLER_PATH中的函数, 当然也可以传入函数。
         },
     ]
 };
